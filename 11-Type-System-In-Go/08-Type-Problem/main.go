d48c03805923152e00d55e6acba883e25ef0c12d
Aditya Hajare <aditya43@gmail.com>
2020-03-30 17:57:15 +0530

    Defined Types or Named Types in Go | Type problem

---
+++ 11-Type-System-In-Go/08-Type-Problem/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		width  uint16
+		height uint16
+	)
+
+	width, height = 255, 265
+	width += 10
+
+	fmt.Printf("width: %d height: %d\n", width, height)
+	fmt.Println("are they equal?", width == height)
+}
