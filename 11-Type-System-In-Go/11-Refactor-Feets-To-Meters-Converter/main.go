544a53466e0af859e86c923537afc131fe4166d8
Aditya Hajare <aditya43@gmail.com>
2020-03-30 18:04:46 +0530

    Defined Types or Named Types in Go | Refactored Feets to Meters converter

---
+++ 11-Type-System-In-Go/11-Refactor-Feets-To-Meters-Converter/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// WHY ADDING OUR OWN TYPES IS IMPORTANT?
+	//
+	// 1. Type-Safety
+	// 2. Increased Readability
+	// 3. Adding Methods to our Types
+	type (
+		Feet   float64
+		Meters float64
+	)
+
+	var (
+		feet   Feet
+		meters Meters
+	)
+
+	arg := os.Args[1]
+
+	// feet is a Feet value now
+	val, _ := strconv.ParseFloat(arg, 64)
+	feet = Feet(val)
+
+	// meters is a Meters value now
+	meters = Meters(feet * 0.3048)
+
+	fmt.Printf("%g feet is %g meters.\n", feet, meters)
+}
