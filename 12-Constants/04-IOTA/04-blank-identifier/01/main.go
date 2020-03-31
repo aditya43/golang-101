0fd7501f51cbbe0b3fb8b3e67e970e34d246821f
Aditya Hajare <aditya43@gmail.com>
2020-03-31 13:42:25 +0530

    IOTA examples

---
+++ 12-Constants/04-IOTA/04-blank-identifier/01/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		EST = -5
+		MST = -7
+		PST = -8
+	)
+
+	fmt.Println(EST, MST, PST)
+}
---
+++ 12-Constants/04-IOTA/04-blank-identifier/02/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		EST = -(5 + iota) // CORRECT: -5
+		MST               // INCORRECT: -6
+		PST               // INCORRECT: -7
+	)
+
+	fmt.Println(EST, MST, PST)
+}
