da02d266b397862e7af660cee41211034404d277
Aditya Hajare <aditya43@gmail.com>
2020-04-01 19:31:40 +0530

    User login

---
+++ 16-Switch-Statements/12-Login/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	usage       = "Usage: [username] [password]"
+	errUser     = "Access denied for %q.\n"
+	errPwd      = "Invalid password for %q.\n"
+	accessOK    = "Access granted to %q.\n"
+	user, user2 = "jack", "adi123"
+	pass, pass2 = "doe", "1986"
+)
+
+func main() {
+	args := os.Args
+
+	if len(args) != 3 {
+		fmt.Println(usage)
+		return
+	}
+
+	u, p := args[1], args[2]
+
+	// More readable, right? 👍
+	switch {
+	case u != user && u != user2:
+		fmt.Printf(errUser, u)
+	case u == user && p == pass:
+		// notice this one (no more duplication)
+		fallthrough
+	case u == user2 && p == pass2:
+		fmt.Printf(accessOK, u)
+	default:
+		fmt.Printf(errPwd, u)
+	}
+}
