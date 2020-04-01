2464b4ea9057b812c4b02e04cb7c045f07b1d378
Aditya Hajare <aditya43@gmail.com>
2020-04-01 19:24:20 +0530

    Switch case - Boolean Expressions

---
+++ 16-Switch-Statements/05-bool-expressions/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 10
+
+	switch {
+	case i > 0:
+		fmt.Println("positive")
+	case i < 0:
+		fmt.Println("negative")
+	default:
+		fmt.Println("zero")
+	}
+}
