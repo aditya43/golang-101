d1f43f96f59f5af6d072c937c76ff6f2fea67802
Aditya Hajare <aditya43@gmail.com>
2020-04-02 08:30:08 +0530

    Loop over words

--- 17-Loops-In-Go/05-for-range/02-loop-over-words/main.go
+++ 17-Loops-In-Go/05-for-range/02-loop-over-words/main.go
@@ -9,23 +9,20 @@ func main() {
 	// this will split the following string by spaces
 	// and then it will put it inside words variable
 	// as a slice of strings
-	words := strings.Fields(
-		"lazy cat jumps again and again and again",
-	)
-	//    1    2    3    4    5     6    7    8
+	words := strings.Fields("🍺 AWS Serverless - Serverless Framework, AWS SAM, Lambda, Step Functions, API Gateway, RDS, DynamoDB, ElephantSQL, CI/CD, CloudFormation, S3 Notifications, SNS, SQS, Cognito, Lambda Crons, CORS, Apache VTL (Velocity Template Language), Swagger, KMS, VPCs, DLQs, CloudWatch, CodeCommit, CodeBuild, CodePipeline, CloudFront, OICD, Kinesis, MQTT, IoT and lot more..")
 
 	// --------------------------------
 	// #1st way:
 	// --------------------------------
 	// for j := 0; j < len(words); j++ {
-	// 	fmt.Printf("#%-2d: %q\n", j+1, words[j])
+	// 	fmt.Printf("#%-5d: %q\n", j+1, words[j])
 	// }
 
 	// --------------------------------
 	// #2nd way (best):
 	// --------------------------------
 	for i, v := range words {
-		fmt.Printf("#%-2d: %q\n", i+1, v)
+		fmt.Printf("#%-5d: %q\n", i+1, v)
 	}
 
 	// --------------------------------
@@ -37,7 +34,7 @@ func main() {
 	// )
 
 	// for i, v = range words {
-	// 	fmt.Printf("#%-2d: %q\n", i+1, v)
+	// 	fmt.Printf("#%-5d: %q\n", i+1, v)
 	// }
 
 	// fmt.Printf("Last value of i and v %d %q\n", i, v)
