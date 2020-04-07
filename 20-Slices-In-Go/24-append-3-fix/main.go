bb75a7ea20a4fa6073c37d279b2fb39f1876f099
Aditya Hajare <aditya43@gmail.com>
2020-04-07 11:01:17 +0530

    Append function | Part 2

---
+++ 20-Slices-In-Go/24-append-3-fix/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	toppings := []string{"black olives", "green peppers"}
+
+	var pizza []string
+	pizza = append(pizza, toppings...)
+	pizza = append(pizza, "onions", "extra cheese")
+
+	fmt.Printf("toppings: %s\n", pizza)
+}
