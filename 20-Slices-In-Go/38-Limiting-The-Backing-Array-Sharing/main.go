62f5067996725df45a489d6aa9c0dcd4caea1dac
Aditya Hajare <aditya43@gmail.com>
2020-04-09 08:02:40 +0530

    Update main.go

--- 20-Slices-In-Go/38-Limiting-The-Backing-Array-Sharing/main.go
+++ 20-Slices-In-Go/38-Limiting-The-Backing-Array-Sharing/main.go
@@ -1,10 +1,16 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/aditya43/golang/20-Slices-In-Go/38-Limiting-The-Backing-Array-Sharing/api"
+)
+
 func main() {
-	// received := api.Read(0, 3)
+	received := api.Read(0, 3)
 
 	received = append(received, []int{1, 3}...)
 
-	// fmt.Println("api.temps     :", api.All())
-	// fmt.Println("main.received :", received)
+	fmt.Println("api.temps     :", api.All())
+	fmt.Println("main.received :", received)
 }
