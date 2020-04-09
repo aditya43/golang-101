16d1f250eefdf9303e905a3f85b777cde6217b9c
Aditya Hajare <aditya43@gmail.com>
2020-04-09 08:00:18 +0530

    API | Limiting the backing array sharing

---
+++ 20-Slices-In-Go/38-Limiting-The-Backing-Array-Sharing/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// received := api.Read(0, 3)
+
+	received = append(received, []int{1, 3}...)
+
+	// fmt.Println("api.temps     :", api.All())
+	// fmt.Println("main.received :", received)
+}
