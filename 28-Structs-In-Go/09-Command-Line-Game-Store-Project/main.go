437748577e3d8e34800b0b095ba4a902475af6a3
Aditya Hajare <aditya43@gmail.com>
2020-04-13 08:55:56 +0530

    Command line game store

---
+++ 28-Structs-In-Go/09-Command-Line-Game-Store-Project/main.go
@@ -0,0 +1,53 @@
+// Warm Up
+//
+//  Build a command-line game store.
+//
+//  1. Declare the following structs:
+//
+//     + item: id (int), name (string), price (int)
+//
+//     + game: embed the item, genre (string)
+//
+//
+//  2. Create a game slice using the following data:
+//
+//     id  name          price    genre
+//
+//     1   god of war    50       action adventure
+//     2   x-com 2       30       strategy
+//     3   minecraft     20       sandbox
+//
+//
+//  3. Print all the games.
+package main
+
+import "fmt"
+
+func main() {
+	type item struct {
+		id    int
+		name  string
+		price int
+	}
+
+	type game struct {
+		item
+		genre string
+	}
+
+	games := []game{
+		{
+			item:  item{id: 1, name: "god of war", price: 50},
+			genre: "action adventure",
+		},
+		{item: item{id: 2, name: "x-com 2", price: 40}, genre: "strategy"},
+		{item: item{id: 3, name: "minecraft", price: 20}, genre: "sandbox"},
+	}
+
+	fmt.Printf("Inanc's game store has %d games.\n\n", len(games))
+
+	for _, g := range games {
+		fmt.Printf("#%d: %-15q %-20s $%d\n",
+			g.id, g.name, "("+g.genre+")", g.price)
+	}
+}
