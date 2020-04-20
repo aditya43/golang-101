1879f62beb498d9dab9f635e51ff927faf238058
Aditya Hajare <aditya43@gmail.com>
2020-04-20 20:38:42 +0530

    Web server in Go

---
+++ 65-Web-Server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/aditya43/golang-core/65-Web-Server/validationkit"
+)
+
+func sayNamasteHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "नमस्ते आदित्य!")
+}
+
+func checkUsernameSyntaxHandler(w http.ResponseWriter, r *http.Request) {
+	var usernameSyntaxResult bool
+	username := r.URL.Query().Get("username")
+
+	if username == "" {
+		http.Error(w, "Username not provided!", http.StatusInternalServerError)
+	} else {
+		usernameSyntaxResult = validationkit.CheckUsernameSyntax(username)
+		fmt.Fprintf(w, "Syntax Check Result for %v is %v", username, usernameSyntaxResult)
+	}
+}
+
+func main() {
+	http.HandleFunc("/say-namaste", sayNamasteHandler)
+	http.HandleFunc("/username-syntax-checker", checkUsernameSyntaxHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
