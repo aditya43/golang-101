5fca794d25853665dc85c149c17184cac0b976c8
Aditya Hajare <aditya43@gmail.com>
2020-04-22 21:23:31 +0530

    Templates in go

--- 66-Web-Templates/main.go
+++ 66-Web-Templates/main.go
@@ -14,24 +14,31 @@ type Gopher struct {
 // Handler for the namaste-user route
 func namasteGopherHandler(w http.ResponseWriter, r *http.Request) {
 	var userName string
+
 	userName = r.URL.Query().Get("userName")
+
 	if userName == "" {
 		userName = "आदित्य" // Default username
 	}
+
 	gopher := Gopher{Name: userName}
+
 	renderTemplate(w, "./templates/namaste.html", gopher)
 }
 
 // Template rendering function
 func renderTemplate(w http.ResponseWriter, templateFile string, templateData interface{}) {
 	t, err := template.ParseFiles(templateFile)
+
 	if err != nil {
 		log.Fatal("Error encountered while parsing the template: ", err)
 	}
+
 	_ = t.Execute(w, templateData)
 }
 
 func main() {
 	http.HandleFunc("/namaste-user", namasteGopherHandler)
+
 	_ = http.ListenAndServe(":8080", nil)
 }
