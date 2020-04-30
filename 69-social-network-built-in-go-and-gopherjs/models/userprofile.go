27dc13fe2bae9b186341109b21ebe0e5e6ea09b2
Aditya Hajare <aditya43@gmail.com>
2020-04-30 19:46:24 +0530

    Social Network built using Golang and GopherJS | MySQL, MongoDB, Redis, TLS, Docker

--- 69-social-network-built-in-go-and-gopherjs/models/userprofile.go
+++ 69-social-network-built-in-go-and-gopherjs/models/userprofile.go
@@ -3,7 +3,7 @@ package models
 import "github.com/aditya43/golang-core/69-social-network-built-in-go-and-gopherjs/forms"
 
 type UserProfile struct {
-	PageTitle        string `json:"pageTitle", bson:"pageTitle"`
+	PageTitle        string `json:"pageTitle" bson:"pageTitle"`
 	UUID             string `json:"uuid" bson:"uuid"`
 	Username         string `json:"username" bson:"username"`
 	About            string `json:"about" bson:"about"`
