ef2babff4b669e7a1436df3d47b3b5b75bc4f896
Aditya Hajare <aditya43@gmail.com>
2020-05-01 18:03:09 +0530

    Passing composite data structures into templates | Slice, Map, Struct, Slice Struct, Struct Slice Struct

--- 75-Passing-Composite-Data-Structure-Into-Templates/01-passSlice.go
+++ 75-Passing-Composite-Data-Structure-Into-Templates/01-passSlice.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 )
 
 func passSlice() {
-	fmt.Print("\n-------------------------------------------\n")
-	fmt.Print("Slice")
-	fmt.Print("\n-------------------------------------------\n")
-
 	names := []string{"Aditya", "Nishi", "John", "Jane"}
 
 	err := tpl.ExecuteTemplate(os.Stdout, "01-slice.adi", names)
