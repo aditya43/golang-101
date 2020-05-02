9bd60faef13df80d9821ac392b81f7faf75fe03f
Aditya Hajare <aditya43@gmail.com>
2020-05-02 19:12:47 +0530

    Pipelining | Passing output of one func into another

--- 78-Pipelining/02_pipeline-example/main.go
+++ 78-Pipelining/02_pipeline-example/main.go
@@ -3,9 +3,9 @@ package main
 
 import (
 	"log"
-	"math"
 	"os"
 	"text/template"
+	"time"
 )
 
 var tpl *template.Template
@@ -14,27 +14,17 @@ func init() {
 	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
 }
 
-func double(x int) int {
-	return x + x
-}
-
-func square(x int) float64 {
-	return math.Pow(float64(x), 2)
-}
-
-func sqRoot(x float64) float64 {
-	return math.Sqrt(x)
+func monthDayYear(t time.Time) string {
+	return t.Format("01-02-2006")
 }
 
 var fm = template.FuncMap{
-	"fdbl":  double,
-	"fsq":   square,
-	"fsqrt": sqRoot,
+	"fdateMDY": monthDayYear,
 }
 
 func main() {
 
-	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 3)
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", time.Now())
 	if err != nil {
 		log.Fatalln(err)
 	}
