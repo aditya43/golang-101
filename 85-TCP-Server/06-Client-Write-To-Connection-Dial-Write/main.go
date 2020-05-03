2fdc01ce418444d527f06543569145708cba624a
Aditya Hajare <aditya43@gmail.com>
2020-05-03 23:19:00 +0530

    TCP Server | Dial Write - To Write To  A Connection

---
+++ 85-TCP-Server/06-Client-Write-To-Connection-Dial-Write/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer conn.Close()
+
+	fmt.Fprintln(conn, "Dialed!")
+}
