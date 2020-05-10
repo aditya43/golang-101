e8a7de88911e45dd50ace145bf5a7e42d32184c9
Aditya Hajare <aditya43@gmail.com>
2020-05-10 19:38:10 +0530

    net/http Package | Response Writer

---
+++ 88-nethttp-Package/04-ResponseWriter/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type hotdog int
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Custom-Aditya-Header-Key", "this is a custom header")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintln(w, "<h1>Any code you want in this func</h1>")
+}
+
+func main() {
+	var d hotdog
+	_ = http.ListenAndServe(":8080", d)
+}
