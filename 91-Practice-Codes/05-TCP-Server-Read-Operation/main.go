b27ad0df96767f3f81e47abde0209a87b8ec1da7
Aditya Hajare <aditya43@gmail.com>
2020-05-11 18:22:12 +0530

    Practice Codes | TCP Server read operation

--- 91-Practice-Codes/05-TCP-Server-Read-Operation/main.go
+++ 91-Practice-Codes/05-TCP-Server-Read-Operation/main.go
@@ -10,23 +10,28 @@ import (
 
 func main() {
 	l, err := net.Listen("tcp", ":8080")
+
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 	defer l.Close()
 
 	for {
 		conn, err := l.Accept()
+
 		if err != nil {
 			log.Println(err)
 			continue
 		}
 
 		scanner := bufio.NewScanner(conn)
+
 		for scanner.Scan() {
 			ln := scanner.Text()
 			fmt.Println(ln)
 		}
+
 		defer conn.Close()
 
 		// we never get here
