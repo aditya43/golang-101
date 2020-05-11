a670c418fbeb369607e7ff93831b8db7b1c2ab21
Aditya Hajare <aditya43@gmail.com>
2020-05-11 09:31:08 +0530

    net/http Package | ServeMux | Using a default ServeMux

---
+++ 89-nethttp-ServeMux-Routing/03-Default-ServeMux/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func home(res http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		// _, _ = io.WriteString(res, "404 Not Found")
+		http.NotFound(res, req)
+		return
+	}
+
+	_, _ = io.WriteString(res, "Home Page")
+}
+
+func login(res http.ResponseWriter, req *http.Request) {
+	_, _ = io.WriteString(res, "Login Page")
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/login", login)
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
