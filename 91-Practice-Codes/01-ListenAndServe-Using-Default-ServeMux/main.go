2e56381aeaf96bf56e0d446c2702d50ef7a287a3
Aditya Hajare <aditya43@gmail.com>
2020-05-12 10:11:07 +0530

    Practice Codes | ListenAndServe Using Default ServeMux

--- 91-Practice-Codes/01-ListenAndServe-Using-Default-ServeMux/main.go
+++ 91-Practice-Codes/01-ListenAndServe-Using-Default-ServeMux/main.go
@@ -9,7 +9,7 @@ func main() {
 	http.HandleFunc("/", foo)
 	http.HandleFunc("/dog/", bar)
 	http.HandleFunc("/me/", myName)
-	http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe(":8080", nil)
 }
 
 func foo(w http.ResponseWriter, req *http.Request) {
