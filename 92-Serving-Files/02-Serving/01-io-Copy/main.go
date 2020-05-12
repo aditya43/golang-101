f933ac57c213be0828927341992015f109e32347
Aditya Hajare <aditya43@gmail.com>
2020-05-12 09:12:02 +0530

    Serving Files

---
+++ 92-Serving-Files/02-Serving/01-io-Copy/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", dog)
+	http.HandleFunc("/toby.jpg", dogPic)
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func dog(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	_, _ = io.WriteString(w, `
+	<img src="/toby.jpg">
+	`)
+}
+
+func dogPic(w http.ResponseWriter, req *http.Request) {
+	f, err := os.Open("toby.jpg")
+	if err != nil {
+		http.Error(w, "file not found", 404)
+		return
+	}
+	defer f.Close()
+
+	_, _ = io.Copy(w, f)
+}
