6eeddbab7874fc1f087dfa5cfe8bc1d60d03c3c9
Aditya Hajare <aditya43@gmail.com>
2020-05-16 19:59:29 +0530

    User signup using Session cookie

---
+++ 100-Sessions/03-Signup/session.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+)
+
+func getUser(req *http.Request) user {
+	var u user
+
+	// get cookie
+	c, err := req.Cookie("session")
+	if err != nil {
+		return u
+	}
+
+	// if the user exists already, get user
+	if un, ok := dbSessions[c.Value]; ok {
+		u = dbUsers[un]
+	}
+	return u
+}
+
+func alreadyLoggedIn(req *http.Request) bool {
+	c, err := req.Cookie("session")
+	if err != nil {
+		return false
+	}
+	un := dbSessions[c.Value]
+	_, ok := dbUsers[un]
+	return ok
+}
