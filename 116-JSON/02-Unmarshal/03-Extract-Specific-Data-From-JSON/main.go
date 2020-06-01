64c5e087e79127a3c95a93870ff944c25d0d7343
Aditya Hajare <aditya43@gmail.com>
2020-06-01 08:07:23 +0530

    JSON | Extracting specific data fields from JSON into Go data structure

--- 116-JSON/02-Unmarshal/03-Extract-Specific-Data-From-JSON/main.go
+++ 116-JSON/02-Unmarshal/03-Extract-Specific-Data-From-JSON/main.go
@@ -7,14 +7,16 @@ import (
 )
 
 type person struct {
-	Name string `json:"name"`
-	City string `json:"city"`
+	Name    string `json:"name"`
+	City    string `json:"city"`
+	State   string `json:"state"`   // Not present in JSON
+	Country string `json:"country"` // NULL in JSON
 }
 
 func main() {
 	var p person
 
-	jsonStr := `{"name":"Aditya","city":"Pune","favourite_bands":["Metallica","Lamb Of God","Manowar"],"phone_number":1234567890,"websites":[{"name":"AdiInviter Pro","url":"http://www.adiinviter.com"},{"name":"Github","url":"https://github.com/aditya43"}]}`
+	jsonStr := `{"name":"Aditya","city":"Pune","country":null,"favourite_bands":["Metallica","Lamb Of God","Manowar"],"phone_number":1234567890,"websites":[{"name":"AdiInviter Pro","url":"http://www.adiinviter.com"},{"name":"Github","url":"https://github.com/aditya43"}]}`
 
 	err := json.Unmarshal([]byte(jsonStr), &p)
 
@@ -24,4 +26,6 @@ func main() {
 
 	fmt.Println(p.Name)
 	fmt.Println(p.City)
+	fmt.Println(p.State)
+	fmt.Println(p.Country)
 }
