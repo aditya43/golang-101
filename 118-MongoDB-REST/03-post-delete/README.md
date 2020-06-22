1dcb5ecd9ed43adafdcfd840801264ebd8c1904e
Aditya Hajare <aditya43@gmail.com>
2020-06-22 09:51:09 +0530

    REST API | Create User and Delete User

---
+++ 118-MongoDB-REST/03-post-delete/README.md
@@ -0,0 +1,29 @@
+# Using Curl
+
+1. Start your server
+
+1. Enter this at the terminal
+
+```
+curl http://localhost:8080/user/1
+```
+
+```
+curl -X POST -H "Content-Type: application/json" -d '{"Name":"James Bond","Gender":"male","Age":32,"Id":"777"}' http://localhost:8080/user
+```
+
+-X is short for --request
+Specifies a custom request method to use when communicating with the HTTP server.
+
+-H is short for --header
+
+-d is short for --data
+
+
+```
+curl -X DELETE -H "Content-Type: application/json" http://localhost:8080/user/777
+```
+
+IMPORTANT:
+Make sure you update your import statements to import packages from the correct location!
+
---
+++ 118-MongoDB-REST/03-post-delete/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/aditya43/golang-core/118-mongodb-rest/03-post-delete/models"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	r := httprouter.New()
+	r.GET("/", index)
+	r.GET("/user/:id", getUser)
+	// added route
+	r.POST("/user", createUser)
+	// added route plus parameter
+	r.DELETE("/user/:id", deleteUser)
+	http.ListenAndServe("localhost:8080", r)
+}
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	s := `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Index</title>
+</head>
+<body>
+<a href="/user/9872309847">GO TO: http://localhost:8080/user/9872309847</a>
+</body>
+</html>
+	`
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(s))
+}
+
+func getUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	u := models.User{
+		Name:   "James Bond",
+		Gender: "male",
+		Age:    32,
+		Id:     p.ByName("id"),
+	}
+
+	// Marshal into JSON
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Write content-type, statuscode, payload
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK) // 200
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	// composite literal - type and curly braces
+	u := models.User{}
+
+	// encode/decode for sending/receiving JSON to/from a stream
+	json.NewDecoder(r.Body).Decode(&u)
+
+	// Change Id
+	u.Id = "007"
+
+	// marshal/unmarshal for having JSON assigned to a variable
+	uj, _ := json.Marshal(u)
+
+	// Write content-type, statuscode, payload
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) // 201
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	// TODO: write code to delete user
+	w.WriteHeader(http.StatusOK) // 200
+	fmt.Fprint(w, "Write code to delete user\n")
+}
