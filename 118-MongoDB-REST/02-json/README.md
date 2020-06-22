32da9c200d42a047bddcf60939e6f62b190171cf
Aditya Hajare <aditya43@gmail.com>
2020-06-22 09:47:48 +0530

    REST API | Get User

---
+++ 118-MongoDB-REST/02-json/README.md
@@ -0,0 +1,8 @@
+# Using Curl
+
+1. Start your server
+
+1. Enter this at the terminal
+```
+curl http://localhost:8080/user/1
+```
---
+++ 118-MongoDB-REST/02-json/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"net/http"
+
+	"github.com/aditya43/golang-core/118-mongodb-rest/02-json/models"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	r := httprouter.New()
+	r.GET("/", index)
+	// added route plus parameter
+	r.GET("/user/:id", getUser)
+	log.Fatalln(http.ListenAndServe("localhost:8080", r))
+}
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	s := `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Index</title>
+</head>
+<body>
+<a href="/user/9872309847">GO TO: http://localhost:8080/user/9872309847</a>
+</body>
+</html>
+	`
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(s))
+}
+
+// changed func name
+func getUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	u := models.User{
+		Name:   "James Bond",
+		Gender: "male",
+		Age:    32,
+		Id:     p.ByName("id"),
+	}
+
+	// Marshal into JSON
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Write content-type, statuscode, payload
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK) // 200
+	fmt.Fprintf(w, "%s\n", uj)
+}
